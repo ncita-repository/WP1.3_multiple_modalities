e19beb90e7fc7a41efc90df693b3653779bb2529
Cristiano <ctorti@gmail.com>
2020-08-03 20:00:43 +0100

    Intersecting points still don't form closed contours

---
+++ Contour_interpolation/GetInputPoints.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 13:09:22 2020
+
+@author: ctorti
+"""
+
+
+"""
+Function:
+    GetInputPoints()
+    
+
+MODIFY THE STUFF BELOW
+    
+Purpose:
+    Create two arrays of contour points (points organised in one of two ways)
+    and a LUT.
+
+
+Input:
+    DicomDir        - Directory containing DICOMs 
+    
+    RoiFpath        - Filepath of the DICOM-RTSTRUCT ROI Collection
+    
+    
+Returns:
+    Pts - Array of points, e.g.:
+                    
+            [
+             [x0, y0, z0],
+             [x1, y1, z1],
+             ...
+             ]
+    
+    PtsBySliceAndContourAndContour - Array of array of array of points organised by 
+                           contour and slice, e.g.
+                
+                [
+                 [ <-- start of array of array of points in slice 0
+                  [x0_c0_s0, y0_c0_s0, z0_c0_s0], [x1_c0_s0, y1_c0_s0, z1_c0_s0], ...], <-- array of points in contour 0 in slice 0
+                  [x0_c1_s0, y0_c1_s0, z0_c1_s0], [x1_c1_s0, y1_c1_s0, z1_c1_s0], ...], <-- array of points in contour 1 in slice 0
+                  ...
+                  ], <-- end of array of array of contour in slice 0
+                 
+                 [ <-- start of array of array of points in slice 1
+                  [x0_c0_s1, y0_c0_s1, z0_c0_s1], [x1_c0_s1, y1_c0_s1, z1_c0_s1], ...], <-- array of points in contour 0 in slice 1
+                  [x0_c1_s1, y0_c1_s1, z0_c1_s1], [x1_c1_s1, y1_c1_s1, z1_c1_s1], ...], <-- array of points in contour 1 in slice 1
+                  ...
+                  ], <-- end of array of array of contour in slice 1
+                 ...
+                 ]
+                 
+    LUT - A P x 5 array (P rows for all P points, 5 columns) containing:
+        
+            - the point number: integer starting from 0, 
+            - the input (fixed image) slice number it came from: integer starting from 0,    (NOTE 08/07/20: THIS WAS PREVIOUSLY IN THE NEXT ROW)
+            - the input (fixed image) contour number it belongs to: integer starting from 0, (NOTE 08/07/20: THIS WAS PREVIOUSLY IN THE PREVIOUS ROW)
+            - an empty list ([]) as a placeholder for the input (fixed image) index of the point: array of integers (N=3),
+            - the input (fixed image) point in PCS: array of floats (N=3)
+            
+            e.g. [0, 
+                  12, 
+                  0, 
+                  [],
+                  [-19.904, -10.938, 2.551]]
+            
+            => The 1st point is part of the 1st contour (the 1st contour in
+            absolute terms, not the 1st of possibly multiple contours in any
+            given slice) that came from the 13th slice, whose coordinates in 
+            the PCS are [-19,904, -10.938, 2.551].
+            
+       - Note that LUT will have items added to it so that it will ultimately
+         be a P x 12 array (P rows for all P points, 12 columns) containing:
+        
+            - (col 0): the point number: integer starting from 0, 
+            - (col 1): the input (fixed image) slice number it came from: integer starting from 0,    (NOTE 08/07/20: THIS WAS PREVIOUSLY IN THE NEXT ROW)
+            - (col 2): the input (fixed image) contour number it belongs to: integer starting from 0, (NOTE 08/07/20: THIS WAS PREVIOUSLY IN THE PREVIOUS ROW)
+            - (col 3): the input (fixed image) index of the point: array of integers (N=3),
+            - (col 4): the input (fixed image) point in PCS: array of floats (N=3),
+            - (col 5): the input (fixed image) point in ICS: array of floats (N=3),
+            - (col 6): the (output / moving image) contour number the transformed point: integer starting from 0,
+            - (col 7): the (output / moving image) index of the transformed point: array of intergers (N=3),
+            - (col 8): the (output / moving image) transformed point in PCS: array of floats (N=3),
+            - (col 9): the (output / moving image) transformed point in ICS: array of floats (N=3),
+            - (col 10): the deformation in PCS: array of floats (N=3),
+            - (col 11): the deformation in ICS: array of floats (N=3)
+
+"""
+
+
+
+#def GetInputPoints(DicomDir, RoiFpath, Origin, Directions, Spacings): # July 24
+def GetInputPoints(FixedDicomDir, FixedRoiFpath):
+    # Import packages:
+    #import os
+    #import SimpleITK as sitk
+    #import pydicom
+    #from GetDicoms import GetDicoms
+    #import importlib
+    #import PCStoICS
+    #importlib.reload(PCStoICS)
+    from PCStoICS import PCStoICS
+    #import RegUtilityFuncs as ruf
+    #import GetContourPtsForDicoms
+    #importlib.reload(GetContourPtsForDicoms)
+    from GetContourPtsForDicoms import GetContourPtsForDicoms
+    from GetImageAttributes import GetImageAttributes
+    
+    
+    # Get the contour points:    
+    PtsPCS, PtsBySliceAndContourPCS, LUT = GetContourPtsForDicoms(FixedDicomDir, 
+                                                                  FixedRoiFpath)
+    
+    P = len(PtsPCS)
+    S = len(PtsBySliceAndContourPCS)
+    
+    # List of slice numbers:
+    SliceNos = list(range(S))
+    
+    
+    # Chose which package to use to get the Image Plane Attributes:
+    #package = 'sitk'
+    package = 'pydicom'
+
+    # Get the Image Attributes for the Fixed image:
+    FixOrigin, FixDirs,\
+    FixSpacings, FixDims = GetImageAttributes(DicomDir=FixedDicomDir, 
+                                              Package=package)
+
+
+    # Convert points in Pts_PCS from PCS to ICS:
+    PtsICS = PCStoICS(Pts_PCS=PtsPCS, Origin=FixOrigin, 
+                      Directions=FixDirs, Spacings=FixSpacings)
+
+    
+    # Initialise list of converted points in PtsBySliceAndContourPCS from PCS 
+    # to ICS:
+    PtsBySliceAndContourICS = []
+    
+    # Initialise ContourTypeBySlice:
+    ContourTypeBySlice = []
+    
+    # Loop through each slice of points:
+    for PtsThisSlicePCS in PtsBySliceAndContourPCS:
+        # If PtsThisSlicePCS is not empty:
+        if PtsThisSlicePCS:
+            # Initialise the converted points for this slice:
+            PtsThisSliceICS = []
+            
+            # Loop through each contour of points:
+            for PtsThisContourPCS in PtsThisSlicePCS:
+                PtsThisContourICS = PCStoICS(Pts_PCS=PtsThisContourPCS, 
+                                             Origin=FixOrigin, 
+                                             Directions=FixDirs, 
+                                             Spacings=FixSpacings)
+        
+                PtsThisSliceICS.append(PtsThisContourICS)
+               
+            PtsBySliceAndContourICS.append(PtsThisSliceICS)
+            
+            ContourTypeBySlice.append(1)
+            
+        else:
+            PtsBySliceAndContourICS.append([])
+            
+            ContourTypeBySlice.append(0)
+            
+    
+    
+    # Convert each PCS point (last item) in LUT and append to the sub-array:
+    for i in range(P):
+        PtPCS = LUT[i][-1]
+        
+        #print('PtPCS =', PtPCS)
+        
+        #PtICS = PCStoICS(Pts_PCS=[PtPCS], Origin=Origin,
+        #                 Directions=Directions, Spacings=Spacings)
+        PtICS = PCStoICS(Pts_PCS=PtPCS, Origin=FixOrigin,
+                         Directions=FixDirs, Spacings=FixSpacings)
+        
+        #print('PtICS =', PtICS[0])
+        
+        #LUT[i].append(PtICS[0])
+        LUT[i].append(PtICS)
+    
+ 
+    
+    # Convert the LUT to a dictionary:
+    PointData = {
+                 'PointNo'       : [LUT[i][0] for i in range(P)], 
+                 'InSliceNo'     : [LUT[i][1] for i in range(P)], 
+                 'InContourNo'   : [LUT[i][2] for i in range(P)],
+                 'InContourType' : [LUT[i][3] for i in range(P)],
+                 'InPointIndex'  : [LUT[i][4] for i in range(P)],
+                 'InPointPCS'    : [LUT[i][5] for i in range(P)],
+                 'InPointICS'    : [LUT[i][6] for i in range(P)]
+                 } 
+    
+    # Create a dictionary to store the contour data for the Fixed image
+    # (input points) arranged by slices and contours:
+    FixContourData = {
+                      'SliceNo'     : SliceNos, 
+                      'ContourType' : ContourTypeBySlice,
+                      'PointPCS'    : PtsBySliceAndContourPCS,
+                      'PointICS'    : PtsBySliceAndContourICS
+                      } 
+    
+          
+    #print(len(SliceNos))
+    #print(len(PtsBySliceAndContourPCS))     
+    
+    return PtsPCS, PtsBySliceAndContourPCS,\
+           PtsICS, PtsBySliceAndContourICS,\
+           LUT, PointData, FixContourData
---
+++ Contour_interpolation/PCStoICS.py
@@ -0,0 +1,350 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 13:58:43 2020
+
+@author: ctorti
+"""
+
+
+
+
+
+"""
+Function:
+    PCStoICS()
+    
+Purpose:
+    Convert points Pts_PCS from Patient Coordinate System (PCS) to Image 
+    Coordinate System (ICS).
+    
+    Pts_PCS are in the form:
+        
+    [[point0_x, point0_y, point0_z],
+     [point1_x, point1_y, point1_z],
+     ...
+     ]
+    
+    The conversion requires the image origin (ImagePositionPatient), the 
+    direction cosines along x, y and z, and the pixel spacings d = [di, dj, dk].
+
+
+Input:
+    Pts_PCS    - Points in Patient Coordinate System 
+    
+    Origin     - The image origin (or ImagePositionPatient) (e.g. [x0, y0, z0])
+    
+    Directions - The direction cosine along x (rows), y (columns) and z (slices)
+                 (e.g. [Xx, Xy, Xz, Yx, Yy, Yz, Zx, Zy, Zz])
+    
+    Spacings   - The pixel spacings along x, y and z (e.g. [di, dj, dk])
+    
+    
+    
+Returns:
+    Pts_PCS - Points in Image Coordinate System
+    
+
+
+
+Equations:
+
+P = S + (di*i).X + (dj*j).Y + (dk*k).Z
+
+where P = (Px, Py, Pz) is the point in the Patient Coordinate System
+
+      S = (Sx, Sy, Sz) is the origin in the Patient Coordinate System (i.e.
+      the ImagePositionPatient)
+      
+      X = (Xx, Xy, Xz) is the direction cosine vector along rows (x) 
+      
+      Y = (Yx, Yy, Yz) is the direction cosine vector along columns (y)
+      
+      Z = (Zx, Zy, Zz) is the direction cosine vector along slices (z)
+      
+      di, dj and dk are the pixel spacings along x, y and z
+      
+      i, j, and k are the row (x), column (y) and slice (z) indices
+      
+      
+Solve for i, j and k, i.e.:
+    
+    Vx = Xx*di*i + Yx*dj*j + Zx*dk*k
+    
+    Vy = Xy*di*i + Yy*dj*j + Zy*dk*k
+    
+    Vz = Xz*di*i + Yz*dk*k + Zz*dk*k
+    
+where Vx = Px - Sx
+
+      Vy = Py - Sy
+      
+      Vz = Pz - Sz
+      
+Solving for i, j and k results in the expressions:
+    
+    i = (1/di)*d/e
+        
+    j = (1/dj)*( (a/b)*di*i + c/b )
+    
+    k = (1/dk)*(1/Zz)*(Vz - Xz*di*i - Yz*dj*j)
+    or
+    k = (1/dk)*(1/Zz)*( Vz - (c/b)*Yz - (Xz + (a/b)*Yz)*di*i )
+    
+    (either expression for k should be fine)
+    
+In the case where X = [1, 0, 0]
+                  Y = [0, 1, 0]
+                  Z = [0, 0, 1]
+                  
+The expressions simplify to:
+    
+    a = 0
+    
+    b = 1
+    
+    c = Vy
+    
+    d = Vx
+    
+    e = 1
+    
+    i = Vx/di
+    
+    j = Vy/dj
+    
+    k = Vz/dk
+    
+"""
+
+
+
+
+
+
+""" Note: 
+    
+    The input Pts_PCS for PCStoICS() needs to be an array of points, 
+    so if only converting a single point, the Pts_PCS must have 
+    square brackets around it to turn it into an array of length 1,
+    
+    e.g. Pts_PCS = [[x,y,z]] for point = [x,y,z]
+    
+    but the output must be indexed with [0],
+    
+    e.g. Pts_ICS[0]
+    
+    to get the point in the form [x,y,z] (otherwise it will be [[x,y,z]]).
+    
+    
+"""
+        
+
+def PCStoICS(Pts_PCS, Origin, Directions, Spacings):
+    # Import packages:
+    #import SimpleITK as sitk
+    
+    """ 
+    Depending on whether Pts_PCS is a list of points or a single point things
+    will need to be dealt with differently. So first determine the number of
+    points in Pts_PCS.
+    """
+    
+    # If Pts_PCS is a list of points, the first item will be a list of 
+    # [x, y, z] coordinates:
+    if isinstance(Pts_PCS[0], list):
+        # Pts_PCS is a list of P points:
+        P = len(Pts_PCS)
+        
+    else:
+        # Pts_PCS is a single point:
+        P = 1
+        
+        # Since the for loop below is intended to act on a list of points, 
+        # convert the point into a list of length 1:
+        Pts_PCS = [Pts_PCS]
+    
+    
+    # Define S, X, Y and Z:
+    S = Origin # the origin
+    
+    X = Directions[0:3] # the row (x) direction cosine vector
+    Y = Directions[3:6] # the column (y) direction cosine vector
+    Z = Directions[6:] # the slice (z) direction cosine vector
+    
+    # The indeces of the largest direction cosines along rows, columns and 
+    # slices:
+    ind_i = X.index(max(X)) # typically = 0
+    ind_j = Y.index(max(Y)) # typically = 1
+    ind_k = Z.index(max(Z)) # typically = 2
+
+    # The pixel spacings:
+    di = Spacings[0]
+    dj = Spacings[1]
+    dk = Spacings[2] 
+    
+    # Simplifying expressions:
+    Xx = X[ind_i]
+    Xy = X[ind_j]
+    Xz = X[ind_k]
+    
+    Yx = Y[ind_i]
+    Yy = Y[ind_j]
+    Yz = Y[ind_k]
+    
+    Zx = Z[ind_i]
+    Zy = Z[ind_j]
+    Zz = Z[ind_k]
+    
+    
+    # Initialise Pts_ICS:
+    Pts_ICS = []
+    
+    # Loop through each pint in Pts_PCS:
+    for p in range(len(Pts_PCS)):
+        # Get the point:
+        point = Pts_PCS[p]
+        
+        #print(f'\npoint = {point}')
+        
+        Vx = point[0] - S[0]
+        Vy = point[1] - S[1]
+        Vz = point[2] - S[2]
+        
+            
+        """ Equations from 17/06 Attempt #2: """
+        # Define simplifying expressions:
+        a = Xz*Zy/Zz - Xy
+        
+        b = Yy - Yz*Zy/Zz
+        
+        c = Vy - Vz*Zy/Zz
+        
+        d = Vx - (c/b)*Yx - (Zx/Zz)*(Vz - (c/b)*Yz)
+        
+        e = Xx + (a/b)*Yx - (Zx/Zz)*(Xz + (a/b)*Yz)
+        
+        # Solve for i, j and k:
+        i = (1/di)*d/e
+        
+        j = (1/dj)*( (a/b)*di*i + c/b )
+        
+        k = (1/dk)*(1/Zz)*(Vz - Xz*di*i - Yz*dj*j)
+        #k = (1/dk)*(1/Zz)*( Vz - (c/b)*Yz - (Xz + (a/b)*Yz)*di*i )
+        """ Above two expressions should be the same """
+
+        
+        #Pts_ICS.append([di*i, dj*j, dk*k])
+        Pts_ICS.append([i, j, k])
+     
+    if P == 1:
+        # Convert Pts_ICS from a list of length 1 to a single point (as was
+        # Pts_PCS):
+        Pts_ICS = Pts_ICS[0]
+    
+    
+    return Pts_ICS
+
+
+    """ PREVIOUS DERIVATIONS: """
+
+    """ 
+    Equations from 15/06 Attempt #3 (same results as 17/06 Attempt #2): 
+    # Define simplifying expressions:
+    a = Yz*Zy/(Yy*Zz) - 1
+    
+    b = Vz*Zy/(Vy*Zz) - 1
+    
+    c = 1 - Xz*Zy/(Xy*Zz)
+    
+    d = 1 - b*Vy*Yz/(a*Vz*Yy)
+    
+    e = 1 + c*Xy*Yz/(a*Xz*Yy)
+    
+    f = 1 + c*Xy*Yx/(a*Xx*Yy) - e*Xz*Zx/(Xx*Zz)
+    
+    g = 1 - b*Vy*Yx/(a*Vx*Yy) - d*Vz*Zx/(Vx*Zz)
+    
+    # Solve for i, j and k:
+    i = g*Vx / (f*Xx*di)
+
+    j = 1/(a*dj*Yy) * (b*Vy + c*Xy*di*i)
+    
+    k = ( 1/(dk*Zz) ) * (Vz - Xz*di*i - Yz*dj*j)
+    """
+        
+    
+        
+    """ 
+    (INCORRECT) Equations from 17/06 Attempt #1:
+    # Define simplifying expressions:
+    a = (Xy - Xz*Zy/Zz) / (Yz*Zy/Zz - Yy)
+                
+    b = (Vz*Zy/Zz - Vy) / (Yz*Zy/Zz - Yy)
+    
+    c = Vx - b*Yx - (Zx/Zz)*(Vz - b*Yz)
+    
+    d = Xx + a*Yx - (Zx/Zz)*(Xz + a*Yz)
+
+    # Solve for i, j and k:
+    i = (1/di)*c/d
+                
+    j = (1/dj)*(a*di*i + b)
+    
+    k = (1/dk)*(1/Zz)*(Vz - b*Yz - (Xz + a*Yz)*di*i)
+    #k = (1/dk)*(1/Zz)*(Vz - di*Xz*i - dj*Yz*j)
+    # Above two expressions should be the same
+    """
+    
+    
+    
+    """ Equations derived for 2D but with approximation for k: 
+    a = 1 - (Xy*Yx)/(Xx*Yy)
+            
+    i = (1/di)*( (Vx - Vy*Yx/Yy) / (a*Xx) )
+    
+    j = (1/dj)*( (Vy - Xy*di*i) / Yy )
+    
+    k = (1/dk)*Vz # approximation    
+    """
+    
+    
+    
+    """ Equations adapted from James Petts' equations: 
+    # The direction cosines along rows and columns:
+    Theta_r = Directions[0:3]
+    Theta_c = Directions[3:6]
+    
+    # The indeces of the largest direction cosines along rows and columns:
+    IndMaxTheta_r = Theta_r.index(max(Theta_r)) # typically = 0
+    IndMaxTheta_c = Theta_c.index(max(Theta_c)) # typically = 1
+    
+    # The pixel spacings:
+    dx = Spacings[0]
+    dy = Spacings[1]
+        
+    v = [point[0] - S[0], point[1] - S[1], point[2] - S[2]]
+         
+    i = (v[IndMaxTheta_r] - v[IndMaxTheta_c]*Theta_c[IndMaxTheta_r]/Theta_c[IndMaxTheta_c]) / \
+    (
+    Theta_r[IndMaxTheta_r]*dx *
+    (1 - (Theta_c[IndMaxTheta_r]*Theta_r[IndMaxTheta_c]) / (Theta_r[IndMaxTheta_r]*Theta_c[IndMaxTheta_c]))
+    )
+    
+    # Equation above copied below but without line breaks:
+    # i = (v[IndMaxTheta_r] - Theta_c[IndMaxTheta_r]*v[IndMaxTheta_c]/Theta_c[IndMaxTheta_c]) / ( Theta_r[IndMaxTheta_r]*dx * (1 - (Theta_c[IndMaxTheta_r]*Theta_r[IndMaxTheta_c]) / (Theta_r[IndMaxTheta_r]*Theta_c[IndMaxTheta_c])) )
+    
+    j = (v[IndMaxTheta_c] - Theta_r[IndMaxTheta_c]*i*dx) / (Theta_c[IndMaxTheta_c]*dy)
+    
+    # Use approximation for j:
+    k = v[2]/dz
+    
+    # Equations above convered to variables used in the function definition above:
+    # i = (Vx - Yx*Vy/Yy) / ( Xx*di * (1 - (Yx*Xy) / (Xx*Yy)) )
+    
+    # j = (Vy - Xy*i*di) / (Yy*dj)
+    
+    # k = Vz/dk
+    
+    # The above equations are identical to the derivations I came up with - see
+    # "Equations derived for 2D but with approximation for k" above.
+    """
